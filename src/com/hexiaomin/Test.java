dc311c57f2d9ce626a15fab2ce35a50c399a13f0
hexiaomin9527 <52341659.qq.com>
2020-03-17 16:29:54 +0800

    测试提交

--- src/com/hexiaomin/Test.java
+++ src/com/hexiaomin/Test.java
@@ -5,4 +5,7 @@ package com.hexiaomin;
  * @date 2020/3/17 0017 - 下午 1:46
  */
 public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello IDEA,My name is Eclipse!");
+    }
 }
